55c3bad4c3a85e6ca6ba7648452333b84e470ea5
DianaMariaG <dianamariag002@gmail.com>
2022-04-21 18:03:55 +0300

    final commit

--- src/main/resources/sql_scripts/Museum.sql
+++ src/main/resources/sql_scripts/Museum.sql
@@ -3,8 +3,8 @@ drop database museum_management;
 create database if not exists museum_management;
 use museum_management;
 
-create table if not exists open_days (id int not null auto_increment primary key,
-name varchar(10) not null);
+# create table if not exists open_days (id int not null auto_increment primary key,
+# name varchar(10) not null);
 
 
 create table if not exists museum (id int not null auto_increment primary key,
@@ -14,11 +14,11 @@ open_time varchar(10) not null,
 close_time varchar(10) not null);
 
 
-create table if not exists museum_open_days (open_days_id int not null,
-museum_id int not null,
-CONSTRAINT PK_Museum_Days PRIMARY KEY (open_days_id, museum_id),
-foreign key (open_days_id) references open_days(id),
-foreign key (museum_id) references museum(id));
+# create table if not exists museum_open_days (open_days_id int not null,
+# museum_id int not null,
+# CONSTRAINT PK_Museum_Days PRIMARY KEY (open_days_id, museum_id),
+# foreign key (open_days_id) references open_days(id),
+# foreign key (museum_id) references museum(id));
 
 
 create table if not exists ticket_type (id int not null auto_increment primary key, 
